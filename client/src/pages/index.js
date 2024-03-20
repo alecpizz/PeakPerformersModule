2b36f906771d55dd65d4df0bbd1d4e384f8dfa05
Alec Pizziferro <alecpizz@gmail.com>
2024-03-20 16:06:57 -0500

    Move mongo text to the main page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,32 +1,14 @@
-import React, { useState } from "react";
 import logo from './cat.jpg';
 
 const Home = () =>
-{
-     const [dbText, setDbText] = useState('');
+{ 
      return (
           <div>
                <button type="button" onClick={(e) =>
                {
-                    e.preventDefault();
-
-                    fetch("http://localhost:5000/mongodb", {
-                         method: "GET"
-                    }).then((response) => response.json()).then((data) =>
-                    {
-                         console.log(data.message);
-                         setDbText(data.message);
-                    }).catch((error) =>
-                    {
-                         console.error("Error fetching data:", error);
-                         setDbText(error.message);
-                    });
-
-
                }}>Query MongoDB</button>
                <h1>Welcome to CAT 3DCP Digital Marketplace</h1>
                <img src={logo} alt="Caterpillar Logo" width="128" height="128"></img>
-               <p>{dbText}</p>
           </div>
      );
 };
--- client/src/pages/product.js
+++ client/src/pages/product.js
@@ -1,14 +1,28 @@
 import React from "react";
+import { useState } from "react";
 
 const Product = () =>
 {
+     const [dbText, setDbText] = useState('');
+     fetch("http://localhost:5000/mongodb", {
+                         method: "GET"
+                    }).then((response) => response.json()).then((data) =>
+                    {
+                         console.log(data.message);
+                         setDbText(data.message);
+                    }).catch((error) =>
+                    {
+                         console.error("Error fetching data:", error);
+                         setDbText(error.message);
+                    });
      return (
           <div>
                <h1>
-                    Product Page Text
+                    Product Name Here
                </h1>
-               <p>Abstract Data here.</p>
+               <p>Product Description here.</p>
                <p>Img gallery here.</p>
+               <p>{dbText}</p>
           </div>
      );
 };
