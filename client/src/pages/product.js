ab56dd8fac4701479aca36958407d01ad139bf78
“IsaacSishere” <isullivan@mail.bradley.edu>
2024-04-08 14:54:15 -0500

    Prototype Image Gallery
    Rough image Gallery for the project

--- client/src/pages/product.js
+++ client/src/pages/product.js
@@ -2,8 +2,64 @@ import React from "react";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 
+import image1 from './Building1.jpg';
+import image2 from './Building2.jpg';
+import image3 from './Building3.jpg';
 
 const Product = () => {
+
+class ImageGallery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentIndex: 0,
+      isMagnified: false
+    };
+  }
+
+  handlePrevClick = () => {
+    this.setState(prevState => ({
+      currentIndex: prevState.currentIndex > 0 ? prevState.currentIndex - 1 : this.props.images.length - 1
+    }));
+  };
+
+  handleNextClick = () => {
+    this.setState(prevState => ({
+      currentIndex: prevState.currentIndex < this.props.images.length - 1 ? prevState.currentIndex + 1 : 0
+    }));
+  };
+
+  toggleMagnification = () => {
+    this.setState(prevState => ({
+      isMagnified: !prevState.isMagnified
+    }));
+  };
+
+  render() {
+    const { images } = this.props;
+    const { currentIndex, isMagnified } = this.state;
+
+    return (
+      <div className="image-gallery">
+        <button onClick={this.handlePrevClick}>Previous</button>
+        <button onClick={this.toggleMagnification}>{isMagnified ? 'Shrink' : 'Magnify'}</button>
+        <img
+          src={images[currentIndex]}
+          alt={`Image ${currentIndex + 1}`}
+          className={isMagnified ? 'magnified' : ''}
+        />
+        <button onClick={this.handleNextClick}>Next</button>
+      </div>
+    );
+  }
+}
+
+const images = [
+  image1,
+  image2,
+  image3
+];
+
      const [structureInfo, setStructureInfo] = useState('');
 
      let { productId } = useParams();
@@ -27,6 +83,10 @@ const Product = () => {
                     <h1>{{ productId }.productId}</h1>
                     <h1>{structureInfo.structure_type}</h1>
                     <p>{structureInfo.price}</p>
+                    <div>
+                    <h2>Image Gallery</h2>
+                     <ImageGallery images={images} />
+                    </div>
                </div>
                <div>
                     <img src={structureInfo.image_main}></img>
